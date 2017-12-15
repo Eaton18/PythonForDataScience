909b210d7588329ebd0eff4c099e1e8d9d072b6c
yitgeng <yitgeng@ebay.com>
2017-12-15 13:22:12 +0800

    finish logistic regression model

--- MachineLearning/LogisticRegression.py
+++ MachineLearning/LogisticRegression.py
@@ -6,21 +6,23 @@ from sklearn.linear_model import RandomizedLogisticRegression as RLR
 
 # init parameters
 # os.getcwd()
-filename = 'Datasets/bankloan.xls'
+filename = '../Datasets/bankloan.xls'
 data = pd.read_excel(filename)
 x = data.iloc[:,:8].as_matrix()   # regressors
 y = data.iloc[:,8].as_matrix()    # labels
 
+features_columns = data.columns[:len(data.columns)-1]
+
 rlr = RLR()  # create a randomized LogisticRegression, feature selection
 rlr.fit(x, y)  # training model
+print(features_columns)
 print(rlr.get_support())  # get feature selection result.
 print(rlr.scores_)  # get each feature score
 
 print(u'RandomizedLogisticRegression feature selection finished.')
-print(u'The effective features are: %s' % ','.join(data.columns[rlr.get_support()]))
+print(u'The effective features are:\n\t %s' % ', '.join(features_columns[rlr.get_support()]))
 
-features = data.columns[:len(data.columns-1)]
-x = data[data.columns[rlr.get_support()]].as_matrix() # selected features
+x = data[features_columns[rlr.get_support()]].as_matrix() # selected features
 
 lr = LR() # create logistic regression model
 lr.fit(x, y) # using effective features training model
